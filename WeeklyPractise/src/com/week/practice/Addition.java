1bdc42606f2729a3fe590ccbd877cb7d3db58bd1
rohit90jagtap <110627257+rohit90jagtap@users.noreply.github.com>
2022-11-19 14:33:52 +0530

    project_added

---
+++ WeeklyPractise/src/com/week/practice/Addition.java
@@ -0,0 +1,12 @@
+package com.week.practice;
+
+public class Addition {
+	public static void addTwoNumber(int number1,int number2) {
+		int sum=number1+number2;
+		System.out.println("Addition of two numbers is "+sum);
+	}
+	public static void main(String[] args) {
+		addTwoNumber(10, 20);
+	}
+
+}
---
+++ WeeklyPractise/src/com/week/practice/Multiplication.java
@@ -0,0 +1,12 @@
+package com.week.practice;
+
+public class Multiplication {
+	public static void multiplicationNumber(int number1,int number2) {
+		int multiplication=number1*number2;
+		System.out.println("Multiplication of two numbers is "+multiplication);
+	}
+	public static void main(String[] args) {
+		multiplicationNumber(10, 20);
+	}
+
+}
